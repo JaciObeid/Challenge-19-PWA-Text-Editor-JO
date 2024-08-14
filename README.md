a5f12ab09f6fa941592b08636fe07589b6000fcb
Jacqueline Obeidzinski <jobeidzinski2@gmail.com>
2024-08-14 02:52:20 -0500

    Added nessesary code for function in the install.js, editor.js, database.js, and others

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# Text Editor Starter Code
+# Challenge-19-PWA-Text-Editor-JO
+This is Jaci Obeidzinski's Challenge for Module 19. This is a Text Editor Progressive Web App allowing you to write anything on the site and it will save it. This application also has an install feature thanks to the webpack pluggin. 
+
+## How to Install
+First you will need install this repos code onto your personal computer. Then you will need to open a terminal in the repo and run an npm i to install the nessesary packages. After that you will need to run a npm start and open a browser to the stated port the application is listening on. 
+
+## License
+MIT license
+
+## Special Thanks
+Thank you to everyone in my class for participating in class allowing me to learn more. 
+Thank you to my instructor Chris Woolcott and the Teaching Assistant Steven Castro-Mercado. 
+
+
+## Questions 
+If you have any questions contact me at my GitHub @JaciObeid
+or email me at jobeidzinski2@gmail.com
 
 ## Starter Code 
 The starter code used for this challenge can be found on GitHub at [Cautious-Meme](https://github.com/coding-boot-camp/cautious-meme)
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,11 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+
+
+
+
 
 module.exports = () => {
   return {
@@ -16,15 +19,60 @@ module.exports = () => {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
-    },
+    },// TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Jate'
+      }),
+     
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
 
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'Jate',
+        description: 'A PWA text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+// TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+         {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+    
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
 };
+
